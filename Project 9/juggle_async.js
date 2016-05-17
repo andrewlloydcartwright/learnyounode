d182248a00e3161e8c3f68c84483b0b2741fe40a
Andrew Cartwright <andrew@Andrews-MacBook-Pro.local>
2016-05-17 15:50:02 -0500

    StackOverflowed, check comments for original code

--- Project 9/juggle_async.js
+++ Project 9/juggle_async.js
@@ -1,14 +1,56 @@
-var http = require('http');
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
 
-var URL1 = process.argv[2],
-	URL2 = process.argv[3],
-	URL3 = process.argv[4];
+function printResults () {
+  for (var i = 0; i < 3; i++)
+    console.log(results[i])
+}
 
-http.get(URL1, URL2, URL3, function (response) {
-	response.setEncoding('utf8');
-	URL1.response.on('data', function (data) {
-		console.log(data, function (dat) {
-			URL2.response.on('data')
-		})
-	});
-});jugju
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err)
+
+      results[index] = data.toString()
+      count++
+
+      if (count == 3)
+        printResults()
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i)
+
+// var http = require('http');
+// var async = require('async');
+
+// var URL1 = process.argv[2],
+// 	URL2 = process.argv[3],
+// 	URL3 = process.argv[4];
+
+// async.series([
+// 	http.get(URL1, function (response) {
+// 		response.setEncoding('utf');
+// 		response.on('data', function cb (data, err) {
+// 			if (err) return cb(err);
+// 			console.log(data);
+// 		});
+// 	});,
+// 	http.get(URL2, function (response) {
+// 		response.setEncoding('utf');
+// 		response.on('data', function(data) {
+// 			console.log(data);
+// 		});
+// 	});,
+// 	http.get(URL3, function (response) {
+// 		response.setEncoding('utf');
+// 		response.on('data', function(data) {
+// 			console.log(data);
+// 		});
+// 	});
+// ]);
